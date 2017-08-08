e74ead5c367258ee18949e59eed23a0b2db7e385
Igor Ilunin <ilunin.igor@gmail.com>
2017-08-08 11:28:16 -0400

    improved producer in-flight msgs

--- nats-streaming-producer.js
+++ nats-streaming-producer.js
@@ -7,7 +7,8 @@ const topic = 'device-data';
 
 const opts = {
     url: 'nats://127.0.0.1:4222',
-    maxPubAcksInflight: 500000,
+    maxPubAcksInflight: 16384,
+   //  maxPubAcksInflight: 100000,
     ackTimeout: 60000
 };
 
@@ -17,31 +18,74 @@ const   stan    = require('node-nats-streaming').connect(cluster_id, client_id, 
 
 const BATCH_SIZE = 100000;
 const NUM_BATCHES = 10;
+let now;
 
 stan
     .on('connect', function () {
         pino.info("STAN connected!");
-        start();
+        now = new Date().getTime();
+        process();
+        // start();
     })
     .on('error', function(reason) {
         pino.error(reason);
     });
 
+let processed = 0;
+const total = BATCH_SIZE * NUM_BATCHES;
 
-async function start() {
+function process(){
+    if(processed <= total){
+        setImmediate(send);
+    }
+    else{
+        process.exit(0);
+    }
+}
+
+function send(){
+    for(processed; processed < total;) {
+        if (stan.pubAckOutstanding >= opts.maxPubAcksInflight) {
+            setTimeout(process, 100);
+            // setImmediate(process);
+            return
+        }
+
+        stan.publish(topic, `{"n":${processed},"ts":${new Date().getTime()}}`, function (err, guid) {
+            if (err) {
+                pino.error(err);
+            } else {
+
+            }
+        }.bind(stan));
+        processed++;
+
+        if (processed % BATCH_SIZE == 0) {
+            pino.info(`${BATCH_SIZE} sent: ${new Date().getTime() - now}`);
+            now = new Date().getTime();
+        }
+    }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function start() {
     let whole_test_now = new Date().getTime();
     for (let i = 0; i < NUM_BATCHES; i++) {
         let now = new Date().getTime();
 
         for (let j = 0; j < BATCH_SIZE; j++) {
-
-            // if(stan.pubAckOutstanding >= opts.maxPubAcksInflight){
-            //     //sleep.msleep(500);
-            //     sleep.msleep(1000);
-            //     // await flush(stan.nc);
-            //     j--;
-            //     continue;
-            // }
+            if(stan.pubAckOutstanding >= opts.maxPubAcksInflight){
+                //sleep.msleep(500);
+                sleep.msleep(5000);
+                // await flush(stan.nc);
+                j--;
+                continue;
+            }
 
             stan.publish(topic, `{"n":${j},"ts":${new Date().getTime()}}`, function(err, guid){
                 if(err) {
